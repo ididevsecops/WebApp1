e74bbde15029d6406b5c9eb3ae9104dd749205fb
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2020-11-19 13:54:05 -0500

    Added logging in Startup.

--- WebApp1/Startup.cs
+++ WebApp1/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace WebApp1
 {
@@ -23,7 +24,7 @@ namespace WebApp1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -37,6 +38,7 @@ namespace WebApp1
                 endpoints.MapGet("/", async context =>
                 {
                     string header = this._config.GetValue<string>("Header");
+                    logger.LogWarning("Hi There");
                     context.Response.Headers.Add("idso", header);
                     await context.Response.WriteAsync("");
                 });
